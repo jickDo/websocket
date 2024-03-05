d783d67bd6eaa1d8c9e4f3f20be72c4550d8865d
jickDo <pocard0117@naver.com>
2024-03-05 14:17:50 +0900

    feature: chatRoom 컨트롤러 구현

---
+++ src/main/java/com/project/websocket/controller/ChatRoomController.java
@@ -0,0 +1,46 @@
+package com.project.websocket.controller;
+
+import com.project.websocket.model.ChatRoom;
+import com.project.websocket.service.ChatService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/chat")
+public class ChatRoomController {
+
+    private final ChatService chatService;
+
+    @GetMapping("/room")
+    public String rooms(Model model) {
+        return "/chat/room";
+    }
+
+    @GetMapping("/rooms")
+    @ResponseBody
+    public List<ChatRoom> room() {
+        return chatService.findAllRoom();
+    }
+
+    @PostMapping("/room")
+    @ResponseBody
+    public ChatRoom createRoom(@RequestParam String name) {
+        return chatService.createRoom(name);
+    }
+
+    @GetMapping("/room/enter/{roomId}")
+    public String roomDetail(Model model, @PathVariable String roomId) {
+        model.addAttribute("roomId", roomId);
+        return "/chat/roomdetail";
+    }
+
+    @GetMapping("/room/{roomId}")
+    @ResponseBody
+    public ChatRoom roomInfo(@PathVariable String roomId) {
+        return chatService.findById(roomId);
+    }
+}
