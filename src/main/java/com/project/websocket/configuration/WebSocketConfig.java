8eab328c94fb820e4c6e6ee3135af2c4db092bd8
jickDo <pocard0117@naver.com>
2024-03-05 19:11:12 +0900

    refactore: 웹소켓 구조 변경

--- src/main/java/com/project/websocket/configuration/WebSocketConfig.java
+++ src/main/java/com/project/websocket/configuration/WebSocketConfig.java
@@ -12,13 +12,14 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws/chat").setAllowedOrigins("*").withSockJS();
+        registry
+                .addEndpoint("/ws")
+                .setAllowedOrigins("*");
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/queue", "topic");
-
-        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/sub");
+        registry.setApplicationDestinationPrefixes("/pub");
     }
 }
